e79b9c8d82ff13b0206b66adde4f1be1aa6f451c
SuryaRamadhaniPerkasa <100654308+SuryaRamadhaniPerkasa@users.noreply.github.com>
2022-03-15 11:12:04 +0700

    pwebf

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>This is a title</title>
+    </head>
+    <body>
+        <h1>Heading 1</h1>
+        <h2>Heading 2</h2>
+        <h3>Heading 3</h3>
+        <h4>Heading 4</h4>
+        <h5>Heading 5</h5>
+        <h6>Heading 6</h6>
+        <p>Hello&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;World!</p>
+        <pre>Hello
+             World!</pre>
+        <pre>
+            <b>bold</b>
+            <strong>strong</strong>
+            <i>italic</i>
+            <u>underline</u>
+            H<sub>2</sub>O
+            x<sup>2</sup>
+            <a href="profile.php" target="_Blank">Link</a>
+            <a href="http:localhost/profile.php">Link</a>
+            <a href="#tips">Jump to Section A</a>
+            <a id="tips">Section A</a>
+
+            <img src="images/Spiderman.jpg">
+        </pre>
+    
+    </body>
+    
+    </html>
